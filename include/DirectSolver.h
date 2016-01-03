f1c1e3635d3be50ed0a0c16a80f485ed90cbf005
davvm <ds_minor@hotmail.com>
2016-01-02 22:22:30 -0800

    Added a DirectSolver

---
+++ include/DirectSolver.h
@@ -0,0 +1,19 @@
+#ifndef DIRECTSOLVER_H
+#define DIRECTSOLVER_H
+
+#include "LinearSolver.h"
+
+// Solves the linear system using a direct method
+template <class Real>
+class DirectSolver : public LinearSolver<Real>
+{
+
+public:
+
+	// perform a linear solve (A * result = rhs):
+	virtual void solve(const SparseMatrix &A, const Vector &rhs, Vector &result) const;
+
+};
+
+
+#endif
---
+++ src/DirectSolver.cpp
@@ -0,0 +1,13 @@
+#include "DirectSolver.h"
+
+template<class Real>
+void DirectSolver<Real>::solve(const SparseMatrix &A, const Vector &rhs, Vector &result) const
+{
+	Eigen::SparseLU< SparseMatrix > solver;
+	solver.analyzePattern(A);
+	solver.factorize(A);
+	result = solver.solve(rhs);
+}
+
+template class DirectSolver<float>;
+template class DirectSolver<double>;
