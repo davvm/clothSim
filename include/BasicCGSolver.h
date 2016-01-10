79320b597939afe4d2df70c75931a7277ebe5deb
davvm <ds_minor@hotmail.com>
2016-01-10 11:14:52 -0800

    Added basic conjugate gradient solver

---
+++ include/BasicCGSolver.h
@@ -0,0 +1,19 @@
+#ifndef BASICCGSOLVER_H
+#define BASICCGSOLVER_H
+
+#include "LinearSolver.h"
+
+// Solves the linear system using a direct method
+template <class Real>
+class BasicCGSolver : public LinearSolver<Real>
+{
+
+public:
+
+	// perform a linear solve (A * result = rhs):
+	virtual void solve(const SparseMatrix &A, const Vector &rhs, Vector &result) const;
+
+};
+
+
+#endif
---
+++ src/BasicCGSolver.cpp
@@ -0,0 +1,13 @@
+#include "BasicCGSolver.h"
+#include <Eigen/IterativeLinearSolvers>
+
+template<class Real>
+void BasicCGSolver<Real>::solve(const SparseMatrix &A, const Vector &rhs, Vector &result) const
+{
+	Eigen::ConjugateGradient<SparseMatrix > cg;
+	cg.compute(A);
+	result = cg.solve(rhs);
+}
+
+template class BasicCGSolver<float>;
+template class BasicCGSolver<double>;
